40fdf3af7bca6c0b921c0ae3b578b82f32e852ea
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-12-10 02:51:28 -0800

    style: fix responsive styling for under development banner

--- src/client/components/Badge.tsx
+++ src/client/components/Badge.tsx
@@ -1,9 +1,9 @@
 export function Badge(props: { message: string }) {
   return (
-    <div className="absolute top-4 w-full flex justify-center">
-      <p className="inline-block bg-white border-2 border-neutral-200/50 px-4 py-2 rounded-lg text-center">
+    <div className="absolute top-4 w-full grid mx-auto px-8">
+      <span className="bg-white border-2 border-neutral-200/50 px-4 py-2 rounded-lg text-center mx-auto">
         {props.message}
-      </p>
+      </span>
     </div>
   );
 }
