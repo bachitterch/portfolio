d81ca3f5543ce0cdffd7dc33d8af350665eb1880
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-12-11 04:36:34 -0800

    style: remove bg and reduce border size

--- src/client/components/Badge.tsx
+++ src/client/components/Badge.tsx
@@ -1,7 +1,7 @@
 export function Badge(props: { message: string }) {
   return (
     <div className="absolute top-4 w-full grid mx-auto px-8">
-      <span className="bg-white border-2 border-neutral-200/50 px-4 py-2 rounded-lg text-center mx-auto">
+      <span className="border border-neutral-200 px-4 py-2 rounded-lg text-center mx-auto">
         {props.message}
       </span>
     </div>
