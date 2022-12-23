f52e1b668e4775277b3016e07674df7e13608d26
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-12-23 03:46:21 -0800

    feat: migrate api router from itty to hono

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,66 +1,53 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { Router } from "itty-router";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 
-import { appRouter } from "./_app";
 import type { Env } from "./env";
 import { createContext } from "./trpc";
 import { Dependencies } from "./utils";
+import { appRouter } from "./_app";
 
-const router = Router();
+const app = new Hono<{ Bindings: Env }>();
 
-function addCORS(v: Response) {
-  v.headers.append("Access-Control-Allow-Origin", "*");
-  v.headers.append("Access-Control-Allow-Methods", "GET, POST");
-  v.headers.append(
+app.use("*", logger());
+app.use("/trpc/*", cors());
+
+app.use("/trpc/*", async (c, next) => {
+  await next();
+  c.res.headers.append("Access-Control-Allow-Origin", "*");
+  c.res.headers.append("Access-Control-Allow-Methods", "GET, POST");
+  c.res.headers.append(
     "Access-Control-Allow-Headers",
     "Authorization,Content-Type",
   );
-  v.headers.append("Access-Control-Max-Age", "600");
-  return v;
-}
+  c.res.headers.append("Access-Control-Max-Age", "600");
 
-router.post("/trpc/*", (request: Request, deps: Dependencies) => {
+  return c.res;
+});
+
+app.get("/trpc/*", c => {
+  const deps = new Dependencies(c.env);
+
   return fetchRequestHandler({
     endpoint: "/trpc",
-    req: request,
+    req: c.req,
     router: appRouter,
     createContext: createContext(deps),
   });
 });
 
-router.get("/trpc/*", (request: Request, deps: Dependencies) => {
+app.post("/trpc/*", c => {
+  const deps = new Dependencies(c.env);
+
   return fetchRequestHandler({
     endpoint: "/trpc",
-    req: request,
+    req: c.req,
     router: appRouter,
     createContext: createContext(deps),
   });
 });
 
-router.all("*", () => new Response("", { status: 404 }));
+app.all("*", () => new Response("", { status: 404 }));
 
-const handler = {
-  fetch: async (request: Request, env: Env) => {
-    if (request.method === "OPTIONS") {
-      return addCORS(new Response("", { status: 204 }));
-    }
-
-    const deps = new Dependencies(env);
-
-    let resp = (await router.handle(request, deps).catch(
-      error =>
-        new Response(error.message || "Server Error", {
-          status: error.status || 500,
-        }),
-    )) as Response;
-
-    /* eslint-disable @typescript-eslint/no-unnecessary-condition */
-    if (!resp) {
-      resp = new Response("", { status: 404 });
-    }
-
-    return addCORS(resp);
-  },
-};
-
-export default handler;
+export default app;
