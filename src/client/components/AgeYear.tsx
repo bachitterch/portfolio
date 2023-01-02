06bd7ab1626a9570e640d8dceb8aa7a6729a27ce
Bachitter Chahal <officialbachitterchahal@gmail.com>
2023-01-02 04:34:12 -0800

    fix: age year output to show diff

--- src/client/components/AgeYear.tsx
+++ src/client/components/AgeYear.tsx
@@ -2,11 +2,11 @@ import dayjs from "dayjs";
 import { useState } from "react";
 
 export function AgeYear() {
-  const currentYear = dayjs().year();
   const birthYear = dayjs().year(2000).month(4).date(17);
 
-  const [year, setYear] = useState(currentYear - birthYear.year());
+  const ageNumber = dayjs().diff(birthYear, "years");
+  const [year, setYear] = useState(ageNumber);
 
-  setInterval(() => setYear(currentYear - birthYear.year()), 3600000000);
+  setInterval(() => setYear(ageNumber), 3600000000);
   return <span>{year}</span>;
 }
